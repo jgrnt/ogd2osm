2c0720d2551a74148476824e71407b8c26f6a43e
Georg Göri <georg@makeclean.org>
2013-04-27 15:57:33 +0200

    add csvInElemtn

--- ogd2osm/inElements/__init__.py
+++ ogd2osm/inElements/__init__.py
@@ -1,1 +1,2 @@
 from ogd2osm.inElements.shape import *
+from ogd2osm.inElements.csvShapeColumn import *
---
+++ ogd2osm/inElements/csvShapeColumn.py
@@ -0,0 +1,35 @@
+
+from .shapefile import *
+from osm import pyosm
+import re
+
+
+class csvShapeColumn:
+
+    def __init__(self,params):
+        self.encoding=params.get('encoding','utf-8')
+        self.pointRE=re.compile("POINT \(([0-9.]*) ([0-9.]+)\)") 
+
+    def gen(self,f):
+        file=f[1]
+        l=file.readline().decode(self.encoding).rstrip()
+        print(l)
+        cols=l.split(",")
+        l=file.readline().decode(self.encoding).rstrip()
+        avid=0
+        while l:
+            avid-=1
+            vals=l.split(",")
+            shapeStr= vals[cols.index("SHAPE")]
+            res=re.search(self.pointRE,shapeStr)
+            if res:
+                
+                n=pyosm.Node({
+                            'lat':res.group(1),
+                            'lon':res.group(2),
+                            'id': str(avid)
+                            },tags={k:v for k,v in zip(cols,vals)  if k  not in ('SHAPE') })
+                yield n
+            else:
+                raise Exception("Unknown shape type %d" % r.shape.type)
+            l=file.readline().decode(self.encoding).rstrip()
